ffbd3dff9b339380399c9096c7d01957cb90f1f1
Soros Wen <wenmingjun0725@gmail.com>
2021-05-17 14:28:17 -0400

    Deleted Unused Package

--- AmazonScrap/scrap/views.py
+++ AmazonScrap/scrap/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from bs4 import BeautifulSoup, SoupStrainer
 import requests
-import time
 
 HEADERS = ({'User-Agent':
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 
