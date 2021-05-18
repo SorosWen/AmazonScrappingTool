46da147de3db937be960c45f07bed3f84ee62c06
Soros Wen <wenmingjun0725@gmail.com>
2021-05-18 14:45:06 -0400

    Hotfix
    Fixed an issue with Amazon Security alert. Updated a demo image.

--- AmazonScrap/scrap/views.py
+++ AmazonScrap/scrap/views.py
@@ -4,8 +4,22 @@ from bs4 import BeautifulSoup, SoupStrainer
 import requests
 
 HEADERS = ({'User-Agent':
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 
+            'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36', 
             'Accept-Language': 'en-US, en;q=0.5'})
+headers = {
+    'authority': 'www.amazon.com',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+}
+
 product_number = 12
 
 def interface(request):
@@ -29,10 +43,10 @@ def interface(request):
 # [url, title, price, rating, review]#####################
 ##########################################################
 def amazon_getSearchResult(product_name):
-    url = 'https://www.amazon.com/s?k=' + product_name.replace(' ', '+')
-    webpage = requests.get(url, headers = HEADERS)
+    url = 'https://www.amazon.com/s?k=' + product_name.replace(' ', '+') + '&ref=nb_sb_noss_1'
+    webpage = requests.get(url, headers = headers)
     soup = BeautifulSoup(webpage.content, "lxml", parse_only=SoupStrainer("a", {'class':'a-link-normal s-no-outline'}))
-    links = soup.find_all("a", attrs={'class':'a-link-normal s-no-outline'})[0: 15]
+    links = soup.find_all("a", attrs={'class':'a-link-normal s-no-outline'})[0:15]
     product_list = []
     count = product_number
     for link in links: 
@@ -48,8 +62,8 @@ def amazon_getSearchResult(product_name):
     return product_list
 
 def amazon_product(url):
-    webpage = requests.get(url, headers=HEADERS)
-    soup = BeautifulSoup(webpage.content, "lxml", parse_only=SoupStrainer("span"))
+    webpage = requests.get(url, headers=headers)
+    soup = BeautifulSoup(webpage.content, "lxml", parse_only=SoupStrainer(["span", "title"]))
     title = get_title(soup)
     price = get_price(soup)
     rating = get_rating(soup)
@@ -63,7 +77,10 @@ def get_title(soup):
         try: 
             title = soup.find("span", attrs={"class": 'a-size-large qa-title-text'}).text.replace('\n', '').replace('\'', '')
         except: 
-            title = "N/A"
+            try: 
+                title = soup.title.string.replace('Amazon.com', '')
+            except:
+                title = "N/A"
     return title
 
 def get_price(soup):
